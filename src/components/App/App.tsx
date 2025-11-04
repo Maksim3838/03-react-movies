ec0398421bc29b094223cb9733522c35896ede35
Maksim <70702008m@gmail.com>
2025-11-04 18:19:29 +0200

    text

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,39 @@
 
-import SearchForm from "../SearchForm/SearchForm";
+
+
 
 export default function App() {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>)=>{
+   event.preventDefault ();
+const form = event.currentTarget;
+const formData = new FormData(form);
 
-  const handleSearch = async (topic: string) => {
-    // Тут будемо виконувати HTTP-запит
-    console.log(topic);
-  };
 
+const username = formData.get(`username`);
+const password = formData.get(`password`);
+
+console.log({username, password});
+
+form.reset();
+
+
+
+
+
+  }
   return (
     <>
-      <SearchForm onSubmit={handleSearch} />
+      <h1>Form</h1>
+
+<form onSubmit={handleSubmit}>
+<div><input type="text" name="username"/></div>
+<div><input type="password" name="password"/></div>
+
+<button type="submit">submit</button>
+
+</form>
+
+
     </>
   );
 }
